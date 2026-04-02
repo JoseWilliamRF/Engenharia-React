eeefc57afbe6b60c5727c53e33d12c3433954c3e
devwillrf <jose.william.199422@gmail.com>
2026-04-02 10:38:48 -0400

    Commit inicial: estrutura limpa

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './styles/App.module.css';
+
+function App() {
+  return (
+    <>
+      <header className="nav"></header>
+    </>
+  );
+}
+
+export default App;
